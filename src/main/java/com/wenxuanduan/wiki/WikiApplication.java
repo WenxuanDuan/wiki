44511283006789d236e42216edc30f083248c479
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-01 10:07:51 -0800

    2-5 modify the startup pattern -- XX WIKI

--- src/main/java/com/wenxuanduan/wiki/WikiApplication.java
+++ src/main/java/com/wenxuanduan/wiki/WikiApplication.java
@@ -7,10 +7,11 @@ import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+ import javax.swing.*;
+
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
-
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(WikiApplication.class);
