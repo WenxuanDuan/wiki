70114142539e67dc8296efae480ba69b0733acd1
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-01 10:47:02 -0800

    2-7 add hello interface test, test.http

--- src/main/java/com/wenxuanduan/wiki/controller/TestController.java
+++ src/main/java/com/wenxuanduan/wiki/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.wenxuanduan.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  @RestController: return a string
@@ -20,5 +21,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 }
