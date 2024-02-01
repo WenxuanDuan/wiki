6c6fb64b5f87563cb49beb94f106a8f09f3a4329
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-01 09:57:23 -0800

    2-5 update the start logs

--- src/main/java/com/wenxuanduan/wiki/WikiApplication.java
+++ src/main/java/com/wenxuanduan/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.wenxuanduan.wiki;
 
+ import org.slf4j.ILoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Start successfully!!");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
