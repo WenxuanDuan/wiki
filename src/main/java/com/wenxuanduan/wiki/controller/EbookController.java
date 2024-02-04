4cd6e7807943c8873f54b0e298f870d3822b9b3a
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-04 15:03:19 -0800

    3-6 complete ebook query interface

---
+++ src/main/java/com/wenxuanduan/wiki/controller/EbookController.java
@@ -0,0 +1,23 @@
+package com.wenxuanduan.wiki.controller;
+
+import com.wenxuanduan.wiki.domain.Ebook;
+import com.wenxuanduan.wiki.service.EbookService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/wenxuanduan/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.wenxuanduan.wiki.service;
+
+import com.wenxuanduan.wiki.domain.Ebook;
+import com.wenxuanduan.wiki.mapper.EbookMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
