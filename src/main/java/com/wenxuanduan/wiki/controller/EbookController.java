7eda35b8198628e1ca34f226a4a355747cd61bcc
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-04 15:13:24 -0800

    3-6 add a general return type CommonResp
    All return value of back-end interface will be CommonResp from now.

--- src/main/java/com/wenxuanduan/wiki/controller/EbookController.java
+++ src/main/java/com/wenxuanduan/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.wenxuanduan.wiki.controller;
 
 import com.wenxuanduan.wiki.domain.Ebook;
+import com.wenxuanduan.wiki.resp.CommonResp;
 import com.wenxuanduan.wiki.service.EbookService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
