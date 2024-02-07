299db6bacc4bafca3182e3c197ee614f05224d40
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-07 15:43:12 -0800

    5-6 config axios interceptors to print request parameter and return results

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,24 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios interceptor
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('request parameter：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('return results：', response);
+    return response;
+}, error => {
+    console.log('return error：', error);
+    return Promise.reject(error);
+});
+
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app')
 
