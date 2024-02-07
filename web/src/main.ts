cfac478ef18213efa249fc6a1aa3ea01e06dc082
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-07 10:52:36 -0800

    5-5 VUE multi-environment configuration
    add dev and prod configuration file
    add multi-environment for build and serve command

--- web/src/main.ts
+++ web/src/main.ts
@@ -13,3 +13,7 @@ const icons : any  = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+console.log('Environment: ', process.env.NODE_ENV);
+console.log('Server-Side: ', process.env.VUE_APP_SERVER);
+
