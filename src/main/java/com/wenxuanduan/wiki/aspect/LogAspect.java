52516738649e008b165563a305abb09b5f553bdd
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-07 16:48:59 -0800

    5-9 add AOP, print time consumption for interface, request parameter and return parameter.
    Remove filter and interceptor.

---
+++ src/main/java/com/wenxuanduan/wiki/aspect/LogAspect.java
@@ -0,0 +1,80 @@
+package com.wenxuanduan.wiki.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    /** define a pointercut */
+    @Pointcut("execution(public * com.wenxuanduan.*.controller..*Controller.*(..))")
+    public void controllerPointcut() {}
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+
+        // start to print request log
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // print request message
+        LOG.info("------------- Started -------------");
+        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("Declaring Type Name: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("Remote Address: {}", request.getRemoteAddr());
+
+        // print request parameter
+        Object[] args = joinPoint.getArgs();
+        // LOG.info("Request Parameter: {}", JSONObject.toJSONString(args));
+
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // exclude long properties and sensitive properties
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("Request Parameter: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // exclude long properties and sensitive properties
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("Return Results: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- Finished, Time Consumed：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
--- src/main/java/com/wenxuanduan/wiki/filter/LogFilter.java
+++ src/main/java/com/wenxuanduan/wiki/filter/LogFilter.java
@@ -1,33 +1,33 @@
-package com.wenxuanduan.wiki.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // log the request message
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter Started -------------");
-        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("Remote Address: {}", request.getRemoteAddr());
-
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter Finished, Time Consumed：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.wenxuanduan.wiki.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // log the request message
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter Started -------------");
+//        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("Remote Address: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter Finished, Time Consumed：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
