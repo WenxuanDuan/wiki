672b2d588e923cea97f9cc8feb3bff40e61be806
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-07 16:16:59 -0800

    5-8 add interceptor, log time consumption for interface

---
+++ src/main/java/com/wenxuanduan/wiki/interceptor/LogInterceptor.java
@@ -0,0 +1,38 @@
+package com.wenxuanduan.wiki.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * interceptor：Spring framework specific
+ * usually used on login authentication, permission authentication and log request /login
+ */
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // log request message
+        LOG.info("------------- LogInterceptor Started -------------");
+        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("Remote Address: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("requestStartTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long startTime = (Long) request.getAttribute("requestStartTime");
+        LOG.info("------------- LogInterceptor Finished, Time Consumed：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
