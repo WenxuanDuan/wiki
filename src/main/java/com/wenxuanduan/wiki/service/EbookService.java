fce23635bdf6e693eb8bfe487d7937f83cd1c0e7
WenxuanDuan <wxuan.duan@gmail.com>
2024-02-08 19:24:01 -0800

    6-4 realize pages in the backend using PageHelper

--- src/main/java/com/wenxuanduan/wiki/service/EbookService.java
+++ src/main/java/com/wenxuanduan/wiki/service/EbookService.java
@@ -1,5 +1,7 @@
 package com.wenxuanduan.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wenxuanduan.wiki.domain.Ebook;
 import com.wenxuanduan.wiki.domain.EbookExample;
 import com.wenxuanduan.wiki.mapper.EbookMapper;
@@ -7,6 +9,8 @@ import com.wenxuanduan.wiki.req.EbookReq;
 import com.wenxuanduan.wiki.resp.EbookResp;
 import com.wenxuanduan.wiki.util.CopyUtil;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,17 +18,23 @@ import java.util.List;
 
 @Service
 public class EbookService {
-
+    
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+        PageHelper.startPage(1,3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total columns: {}", pageInfo.getTotal());
+        LOG.info("Total pages: {}", pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
