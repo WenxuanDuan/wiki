90a7e60c8ac5faa0bf6a7ea837d18e135aae68a6
WenxuanDuan <wxuan.duan@gmail.com>
2024-03-12 10:03:48 -0700

    9-9 integrate redis, put sign-on information into redis after login

--- src/main/java/com/wenxuanduan/wiki/controller/UserController.java
+++ src/main/java/com/wenxuanduan/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wenxuanduan.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wenxuanduan.wiki.req.UserLoginReq;
 import com.wenxuanduan.wiki.req.UserQueryReq;
 import com.wenxuanduan.wiki.req.UserResetPasswordReq;
@@ -9,17 +10,31 @@ import com.wenxuanduan.wiki.resp.PageResp;
 import com.wenxuanduan.wiki.resp.UserLoginResp;
 import com.wenxuanduan.wiki.resp.UserQueryResp;
 import com.wenxuanduan.wiki.service.UserService;
+import com.wenxuanduan.wiki.util.SnowFlake;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -57,6 +72,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("Generate single sign-on token：{}，and put it into redis", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
