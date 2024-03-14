cb006869c0417e3bb77c6eab7f492505a4954c77
WenxuanDuan <wxuan.duan@gmail.com>
2024-03-13 20:20:30 -0700

    9-13 do not display menu without login

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -25,13 +25,13 @@
       <a-menu-item key="/">
         <router-link to="/">Home</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/user">User Management</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/ebook">Ebook Management</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
+      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/category">Category Management</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
