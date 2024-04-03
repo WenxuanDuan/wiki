b2b96cf3f690803ad6530753a70b9003f62de26b
WenxuanDuan <wxuan.duan@gmail.com>
2024-04-03 10:34:15 -0700

    11-7 Extract a component for welcome words

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Welcome to XXWIKI</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-welcome',
+  setup () {
+
+    return {
+
+    }
+  }
+});
+</script>
