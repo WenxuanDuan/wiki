329d4d36e350f5d02dbfa2b931d9259785fa3c58
WenxuanDuan <wxuan.duan@gmail.com>
2024-04-05 13:17:29 -0700

    11-10 Optimization: display the button for uploading images in rich text box

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -261,9 +261,11 @@ export default defineComponent({
 .tip {
   padding: 10px 5px;
   margin-bottom: 20px;
-  color: grey;
   border: 1px solid transparent;
   background: linear-gradient(white,white) padding-box,repeating-linear-gradient(-45deg, black 0, black 25%, white 0, white 50%) 0/.6em .6em;
   animation:ants 12s linear infinite;
+}
+.tip b{
+  color: grey;
 }
 </style>
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -199,6 +199,11 @@
       const editor = new E('#content');
       editor.config.zIndex = 0;
 
+      // display the button for uploading images
+      // 上传图片文档：https://doc.wangeditor.com/pages/07-%E4%B8%8A%E4%BC%A0%E5%9B%BE%E7%89%87/01-%E9%85%8D%E7%BD%AE%E6%9C%8D%E5%8A%A1%E7%AB%AF%E6%8E%A5%E5%8F%A3.html
+      // 上传视频文档：https://doc.wangeditor.com/pages/07-%E4%B8%8A%E4%BC%A0%E8%A7%86%E9%A2%91/01-%E9%85%8D%E7%BD%AE%E6%9C%8D%E5%8A%A1%E7%AB%AF%E6%8E%A5%E5%8F%A3.html
+      editor.config.uploadImgShowBase64 = true;
+
       const handleSave = () => {
         confirmLoading.value = true;
         doc.value.content = editor.txt.html();
